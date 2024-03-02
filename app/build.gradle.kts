b163302291084ab28056bd8fe6532845af346bbe
md <mehrdad.dorari@gmail.com>
2024-03-02 17:57:40 +1100

    Added GenderScreen and AgeScreen.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,9 +45,6 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-    hilt {
-        enableAggregatingTask = false
-    }
 }
 
 dependencies {
--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -3,10 +3,15 @@ package com.plcoding.calorytracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -14,50 +19,66 @@ import androidx.navigation.compose.rememberNavController
 import com.plcoding.calorytracker.navigation.navigate
 import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
 import com.plcoding.core.navigation.Route
+import com.plcoding.onboarding_presentation.age.AgeScreen
+import com.plcoding.onboarding_presentation.gender.GenderScreen
 import com.plcoding.onboarding_presentation.welcome.WelcomeScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CaloryTrackerTheme {
                 val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination = Route.WELCOME
-                ){
-                    composable(Route.WELCOME){
-                        WelcomeScreen(onNavigate = navController::navigate)
-                    }
-                    composable(Route.AGE){
+                val scaffoldState = rememberScaffoldState()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState
+                ) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.WELCOME,
+                        modifier = Modifier.padding(paddingValues = it)
+                    ){
+                        composable(Route.WELCOME){
+                            WelcomeScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.GENDER){
+                            GenderScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.AGE){
+                            AgeScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+
+                        }
+                        composable(Route.HEIGHT){
+
+                        }
+                        composable(Route.WEIGHT){
+
+                        }
+                        composable(Route.NUTRIENT_GOAL){
+
+                        }
+                        composable(Route.ACTIVITY){
+
+                        }
+                        composable(Route.GOAL){
+
+                        }
+                        composable(Route.TRACKER_OVERVIEW){
+
+                        }
+                        composable(Route.SEARCH){
+
+                        }
 
                     }
-                    composable(Route.GENDER){
-
-                    }
-                    composable(Route.HEIGHT){
-
-                    }
-                    composable(Route.WEIGHT){
-
-                    }
-                    composable(Route.NUTRIENT_GOAL){
-
-                    }
-                    composable(Route.ACTIVITY){
-
-                    }
-                    composable(Route.GOAL){
-
-                    }
-                    composable(Route.TRACKER_OVERVIEW){
-
-                    }
-                    composable(Route.SEARCH){
-
-                    }
-
                 }
+
             }
         }
     }
